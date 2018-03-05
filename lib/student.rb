49b2d6b63ef5148822fe72822cdb0b48cfc0570d
Judy McGoldrick <jjeanius@verizon.net>
2018-03-05 22:20:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ end
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do|attribute|
-      students_hash << attribute.attribute
+      students_hash << attribute
     end
   end
 
